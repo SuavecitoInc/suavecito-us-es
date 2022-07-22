2b1420c162a1e22a7fc1cf2843f5c15b8f400f37
Jonathan Picazo <jonathanpicazo@outlook.com>
2022-07-22 12:33:51 -0700

    added sub transition

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -39,107 +39,97 @@ export function MobileNav({
   };
 
   return (
-    <>
-      <Transition
-        className={'md:hidden'}
-        show={isMobileOpen}
-        enter="ease-in"
-        enterFrom="translate-y-100 opacity-0 duration-100"
-        enterTo="translate-y-0 opacity-100"
-        leave="ease-out"
-        leaveFrom="translate-y-0 opacity-100"
-        leaveTo="translate-y-100 opacity-0"
-      >
-        <Transition
-          show={currentSubCollection !== null ? false : true}
-          enter="ease-in-out"
-          enterFrom="translate-y-0 opacity-0"
-          enterTo="translate-y-0 opacity-100"
-          leave="ease-out"
-          leaveFrom="translate-y-0 opacity-100"
-          leaveTo="translate-y-0 opacity-0"
-        >
-          <div>
-            <ul className="flex flex-col">
-              {(menu?.items || []).map((item: EnhancedMenuItem) => {
-                if (item.items.length === 0) {
-                  return (
-                    <li
-                      key={item.id}
-                      className="px-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
-                    >
-                      <Link
-                        className="hover:underline text-[15px]"
-                        to={item.to}
-                        target={item.target}
-                      >
-                        {item.title}
-                      </Link>
-                    </li>
-                  );
-                }
+    <Transition
+      className={'md:hidden'}
+      show={isMobileOpen}
+      enter="transform transition ease-in-out duration-500"
+      enterFrom="-translate-y-full"
+      enterTo="translate-y-0"
+      leave="transform transition ease-in-out duration-500"
+      leaveFrom="translate-y-0"
+      leaveTo="-translate-y-full"
+    >
+      <Transition show={currentSubCollection !== null ? false : true}>
+        <nav className={`text-${fillColor}`}>
+          <ul className="flex flex-col">
+            {(menu?.items || []).map((item: EnhancedMenuItem) => {
+              if (item.items.length === 0) {
                 return (
                   <li
                     key={item.id}
                     className="px-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
                   >
-                    <button
-                      className="hover:underline text-[15px] flex justify-between w-full"
-                      onClick={(evt) => toggleSubMenu(item)}
+                    <Link
+                      className="hover:underline text-[15px]"
+                      to={item.to}
+                      target={item.target}
+                      onClick={(evt) => setMobileOpen(false)}
                     >
-                      <span>{item.title}</span>
-                      <span>
-                        <IconArrow direction="right" fill={fillColor} />
-                      </span>
-                    </button>
+                      {item.title}
+                    </Link>
                   </li>
                 );
-              })}
-            </ul>
-          </div>
-        </Transition>
-        <Transition
-          show={currentSubCollection === null ? false : true}
-          enter="ease-in-out"
-          enterFrom="translate-y-0 opacity-0"
-          enterTo="translate-y-0 opacity-100"
-          leave="ease-out"
-          leaveFrom="translate-y-0 opacity-100"
-          leaveTo="translate-y-0 opacity-0"
-        >
-          {currentSubCollection && (
-            <ul className="flex flex-col">
-              <li className="text-[16px] border border-solid border-color-gray-300">
-                <div className="flex items-center">
-                  <button
-                    className="px-[21px] py-[15px] border border-solid border-color-gray-300"
-                    onClick={(evt) => untoggleSubMenu()}
-                  >
-                    <IconArrow direction="left" fill={fillColor} />
-                  </button>
-                  <span className="px-[18px]">
-                    {currentSubCollection.title}
-                  </span>
-                </div>
-              </li>
-              {currentSubCollection.items.map((subItem) => (
+              }
+              return (
                 <li
-                  key={subItem.id}
-                  className="pl-[70px] pr-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
+                  key={item.id}
+                  className="px-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
                 >
-                  <Link
-                    className="hover:underline text-[15px] flex w-full"
-                    to={subItem.to}
-                    target={subItem.target}
+                  <button
+                    className="hover:underline text-[15px] flex justify-between w-full"
+                    onClick={(evt) => toggleSubMenu(item)}
                   >
-                    {subItem.title}
-                  </Link>
+                    <span>{item.title}</span>
+                    <span>
+                      <IconArrow direction="right" fill={fillColor} />
+                    </span>
+                  </button>
                 </li>
-              ))}
-            </ul>
-          )}
-        </Transition>
+              );
+            })}
+          </ul>
+        </nav>
       </Transition>
-    </>
+      <Transition
+        show={currentSubCollection === null ? false : true}
+        enter="transform transition ease-in-out duration-300"
+        enterFrom="-translate-x-full"
+        enterTo="translate-x-0"
+        leave="transform transition ease-in-out duration-300"
+        leaveFrom="translate-x-0"
+        leaveTo="-translate-x-full"
+      >
+        {currentSubCollection && (
+          <ul className="flex flex-col">
+            <li className="text-[16px] border border-solid border-color-gray-300">
+              <div className="flex items-center">
+                <button
+                  className="px-[21px] py-[15px] border border-solid border-color-gray-300"
+                  onClick={(evt) => untoggleSubMenu()}
+                >
+                  <IconArrow direction="left" fill={fillColor} />
+                </button>
+                <span className="px-[18px]">{currentSubCollection.title}</span>
+              </div>
+            </li>
+            {currentSubCollection.items.map((subItem) => (
+              <li
+                key={subItem.id}
+                className="pl-[70px] pr-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
+              >
+                <Link
+                  className="hover:underline text-[15px] flex w-full"
+                  to={subItem.to}
+                  target={subItem.target}
+                  onClick={(evt) => setMobileOpen(false)}
+                >
+                  {subItem.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </Transition>
+    </Transition>
   );
 }
