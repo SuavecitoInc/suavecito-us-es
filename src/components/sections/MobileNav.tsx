96109dd76b51ed5628effceb6165d3e53f3fda5d
j-Riv <jose.alfredo.rivera@gmail.com>
2022-08-16 10:34:54 -0700

    Updates

--- src/components/sections/MobileNav.tsx
+++ src/components/sections/MobileNav.tsx
@@ -28,9 +28,9 @@ export function MobileNav({
     setCurrentSubCollection(item);
   };
 
-  useEffect(() => {
-    setMobileOpen(false);
-  }, [setMobileOpen]);
+  // useEffect(() => {
+  //   setMobileOpen(false);
+  // }, [setMobileOpen]);
 
   const windowSize = useWindowSize().width;
 
@@ -51,21 +51,33 @@ export function MobileNav({
   };
 
   return (
-    <div className={`${!isMobileOpen ? 'hidden' : ''}`}>
+    <div
+      className={`relative ${!isMobileOpen ? 'hidden' : ''}`}
+      style={{
+        minHeight: `${height}px`,
+      }}
+    >
       <Transition
         className={`md:hidden ${themeText[theme!]}`}
-        style={{
-          minHeight: `${height}px`,
-        }}
+        // appear={true}
+        // show={true}
         show={isMobileOpen}
-        enter="transform transition-all ease-in-out duration-300"
-        enterFrom="-translate-y-full opacity-0"
-        enterTo="translate-y-0 opacity-100"
-        leave="transform transition-all ease-in-out duration-300"
-        leaveFrom="translate-y-0 opacity-100"
-        leaveTo="-translate-y-full opacity-0"
+        enter="transition ease-in-out duration-150 transform"
+        enterFrom="-translate-y-full"
+        enterTo="translate-y-0"
+        leave="transition ease-in-out duration-150 transform"
+        leaveFrom="translate-y-0"
+        leaveTo="-translate-y-full"
       >
-        <Transition show={currentSubCollection !== null ? false : true}>
+        <Transition
+          show={currentSubCollection !== null ? false : true}
+          enter="transition ease-in-out duration-300 transform"
+          enterFrom="-translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-full"
+        >
           <nav>
             <ul className="flex flex-col">
               {(menu?.items || []).map((item: EnhancedMenuItem) => {
@@ -114,12 +126,12 @@ export function MobileNav({
         <Transition
           ref={ref}
           show={currentSubCollection === null ? false : true}
-          enter="transform transition-all ease-in-out duration-300"
-          enterFrom="-translate-x-full opacity-0"
-          enterTo="translate-x-0 opacity-100 max-h-fit"
-          leave="transform transition-all ease-in-out duration-300"
-          leaveFrom="translate-x-0 opacity-100 max-h-fit"
-          leaveTo="-translate-x-full opacity-0"
+          enter="transition ease-in-out duration-300 transform absolute top-0 left-0 right-0 w-full"
+          enterFrom="translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform absolute top-0 left-0 right-0 w-full"
+          leaveFrom="translate-x-0"
+          leaveTo="translate-x-full"
         >
           {currentSubCollection && (
             <ul className="flex flex-col">
@@ -162,4 +174,241 @@ export function MobileNav({
       </Transition>
     </div>
   );
+
+  // return (
+  //   <div
+  //     className={`relative ${!isMobileOpen ? 'hidden' : ''}`}
+  //     style={{
+  //       minHeight: `${height}px`,
+  //     }}
+  //   >
+  //     <Transition
+  //       className={`md:hidden ${themeText[theme!]}`}
+  //       appear={true}
+  //       show={true}
+  //       // enter="transition ease-in-out duration-300 transform"
+  //       // enterFrom="-translate-x-full"
+  //       // enterTo="translate-x-0"
+  //       // leave="transition ease-in-out duration-300 transform"
+  //       // leaveFrom="translate-x-0"
+  //       // leaveTo="-translate-x-full"
+  //     >
+  //       <Transition
+  //         show={currentSubCollection !== null ? false : true}
+  //         enter="transition ease-in-out duration-300 transform"
+  //         enterFrom="-translate-x-full"
+  //         enterTo="translate-x-0"
+  //         leave="transition ease-in-out duration-300 transform"
+  //         leaveFrom="translate-x-0"
+  //         leaveTo="-translate-x-full"
+  //       >
+  //         <nav>
+  //           <ul className="flex flex-col">
+  //             {(menu?.items || []).map((item: EnhancedMenuItem) => {
+  //               if (item.items.length === 0) {
+  //                 return (
+  //                   <li
+  //                     key={item.id}
+  //                     className="px-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
+  //                   >
+  //                     <Link
+  //                       className="hover:underline text-[15px]"
+  //                       to={item.to}
+  //                       target={item.target}
+  //                       onClick={(evt) => setMobileOpen(false)}
+  //                     >
+  //                       {item.title}
+  //                     </Link>
+  //                   </li>
+  //                 );
+  //               }
+  //               return (
+  //                 <li
+  //                   key={item.id}
+  //                   className="px-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
+  //                 >
+  //                   <button
+  //                     className="hover:underline text-[15px] flex justify-between w-full"
+  //                     onClick={(evt) => toggleSubMenu(item)}
+  //                   >
+  //                     <span>{item.title}</span>
+  //                     <span>
+  //                       <IconArrow
+  //                         direction="right"
+  //                         theme={theme}
+  //                         width={'w-[14px]'}
+  //                         height={'w-[14px]'}
+  //                       />
+  //                     </span>
+  //                   </button>
+  //                 </li>
+  //               );
+  //             })}
+  //           </ul>
+  //         </nav>
+  //       </Transition>
+  //       <Transition
+  //         ref={ref}
+  //         show={currentSubCollection === null ? false : true}
+  //         enter="transition ease-in-out duration-300 transform absolute top-0 left-0 right-0 w-full"
+  //         enterFrom="translate-x-full"
+  //         enterTo="translate-x-0"
+  //         leave="transition ease-in-out duration-300 transform absolute top-0 left-0 right-0 w-full"
+  //         leaveFrom="translate-x-0"
+  //         leaveTo="translate-x-full"
+  //       >
+  //         {currentSubCollection && (
+  //           <ul className="flex flex-col">
+  //             <li className="text-[16px] border border-solid border-color-gray-300">
+  //               <div className="flex items-center">
+  //                 <button
+  //                   className="px-[21px] py-[15px] border border-solid border-color-gray-300"
+  //                   onClick={(evt) => untoggleSubMenu()}
+  //                 >
+  //                   <IconArrow
+  //                     direction="left"
+  //                     theme={theme}
+  //                     width={'w-[14px]'}
+  //                     height={'w-[14px]'}
+  //                   />
+  //                 </button>
+  //                 <span className="px-[18px]">
+  //                   {currentSubCollection.title}
+  //                 </span>
+  //               </div>
+  //             </li>
+  //             {currentSubCollection.items.map((subItem) => (
+  //               <li
+  //                 key={subItem.id}
+  //                 className="pl-[70px] pr-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
+  //               >
+  //                 <Link
+  //                   className="hover:underline text-[15px] flex w-full"
+  //                   to={subItem.to}
+  //                   target={subItem.target}
+  //                   onClick={(evt) => setMobileOpen(false)}
+  //                 >
+  //                   {subItem.title}
+  //                 </Link>
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         )}
+  //       </Transition>
+  //     </Transition>
+  //   </div>
+  // );
+
+  // return (
+  //   <div className={`${!isMobileOpen ? 'hidden' : ''}`}>
+  //     <Transition
+  //       className={`md:hidden ${themeText[theme!]}`}
+  //       style={{
+  //         minHeight: `${height}px`,
+  //       }}
+  //       show={isMobileOpen}
+  //       enter="transform transition-all ease-in-out duration-300"
+  //       enterFrom="-translate-y-full opacity-0"
+  //       enterTo="translate-y-0 opacity-100"
+  //       leave="transform transition-all ease-in-out duration-300"
+  //       leaveFrom="translate-y-0 opacity-100"
+  //       leaveTo="-translate-y-full opacity-0"
+  //     >
+  //       <Transition show={currentSubCollection !== null ? false : true}>
+  //         <nav>
+  //           <ul className="flex flex-col">
+  //             {(menu?.items || []).map((item: EnhancedMenuItem) => {
+  //               if (item.items.length === 0) {
+  //                 return (
+  //                   <li
+  //                     key={item.id}
+  //                     className="px-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
+  //                   >
+  //                     <Link
+  //                       className="hover:underline text-[15px]"
+  //                       to={item.to}
+  //                       target={item.target}
+  //                       onClick={(evt) => setMobileOpen(false)}
+  //                     >
+  //                       {item.title}
+  //                     </Link>
+  //                   </li>
+  //                 );
+  //               }
+  //               return (
+  //                 <li
+  //                   key={item.id}
+  //                   className="px-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
+  //                 >
+  //                   <button
+  //                     className="hover:underline text-[15px] flex justify-between w-full"
+  //                     onClick={(evt) => toggleSubMenu(item)}
+  //                   >
+  //                     <span>{item.title}</span>
+  //                     <span>
+  //                       <IconArrow
+  //                         direction="right"
+  //                         theme={theme}
+  //                         width={'w-[14px]'}
+  //                         height={'w-[14px]'}
+  //                       />
+  //                     </span>
+  //                   </button>
+  //                 </li>
+  //               );
+  //             })}
+  //           </ul>
+  //         </nav>
+  //       </Transition>
+  //       <Transition
+  //         ref={ref}
+  //         show={currentSubCollection === null ? false : true}
+  //         enter="transform transition-all ease-in-out duration-300"
+  //         enterFrom="-translate-x-full opacity-0"
+  //         enterTo="translate-x-0 opacity-100 max-h-fit"
+  //         leave="transform transition-all ease-in-out duration-300"
+  //         leaveFrom="translate-x-0 opacity-100 max-h-fit"
+  //         leaveTo="-translate-x-full opacity-0"
+  //       >
+  //         {currentSubCollection && (
+  //           <ul className="flex flex-col">
+  //             <li className="text-[16px] border border-solid border-color-gray-300">
+  //               <div className="flex items-center">
+  //                 <button
+  //                   className="px-[21px] py-[15px] border border-solid border-color-gray-300"
+  //                   onClick={(evt) => untoggleSubMenu()}
+  //                 >
+  //                   <IconArrow
+  //                     direction="left"
+  //                     theme={theme}
+  //                     width={'w-[14px]'}
+  //                     height={'w-[14px]'}
+  //                   />
+  //                 </button>
+  //                 <span className="px-[18px]">
+  //                   {currentSubCollection.title}
+  //                 </span>
+  //               </div>
+  //             </li>
+  //             {currentSubCollection.items.map((subItem) => (
+  //               <li
+  //                 key={subItem.id}
+  //                 className="pl-[70px] pr-[30px] py-[15px] text-[16px] border border-solid border-color-gray-300"
+  //               >
+  //                 <Link
+  //                   className="hover:underline text-[15px] flex w-full"
+  //                   to={subItem.to}
+  //                   target={subItem.target}
+  //                   onClick={(evt) => setMobileOpen(false)}
+  //                 >
+  //                   {subItem.title}
+  //                 </Link>
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         )}
+  //       </Transition>
+  //     </Transition>
+  //   </div>
+  // );
 }
