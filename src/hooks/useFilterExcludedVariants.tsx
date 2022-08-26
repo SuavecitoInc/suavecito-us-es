1a293876265062afe00689e891d929a684da443c
j-Riv <jose.alfredo.rivera@gmail.com>
2022-08-25 18:30:51 -0700

    Updated `useFilterExcludedVariants` hook

--- src/hooks/useFilterExcludedVariants.tsx
+++ src/hooks/useFilterExcludedVariants.tsx
@@ -1,3 +1,4 @@
+import {useMemo} from 'react';
 import {ProductVariant} from '@shopify/hydrogen/storefront-api-types';
 
 export function useFilterExcludedVariants(
@@ -7,35 +8,39 @@ export function useFilterExcludedVariants(
     values: string[];
   }[],
 ) {
-  // deep clone
-  const filteredVariants = JSON.parse(JSON.stringify(variants));
-  const filteredOptions = JSON.parse(JSON.stringify(options));
-  // get excluded variant options
-  const excludedOptions: string[] = [];
-  const excludedVariantIds: string[] = [];
-  // @ts-ignore
-  variants?.forEach((variant: ProductVariant) => {
+  const excludedVariantIds = useMemo(() => {
+    // deep clone
+    // const filteredVariants = JSON.parse(JSON.stringify(variants));
+    // const filteredOptions = JSON.parse(JSON.stringify(options));
+    // get excluded variant options
+    const excludedOptions: string[] = [];
+    const excludedVariantIds: string[] = [];
     // @ts-ignore
-    if (variant.variantExcluded && variant.variantExcluded.value === 'true') {
-      excludedVariantIds.push(variant.id);
-      if (variant.selectedOptions[0])
-        excludedOptions.push(variant.selectedOptions[0].value);
-      if (variant.selectedOptions[1])
-        excludedOptions.push(variant.selectedOptions[1].value);
-    }
-  });
-  // remove variants
-  // filteredVariants?.filter(
-  //   (variant: ProductVariant) => !excludedVariantIds.includes(variant.id),
-  // );
-  // remove options from options
-  // @ts-ignore
-  options?.forEach((option: {name: string; values: string[]}) => {
-    const filteredValues = option.values.filter(
-      (value: string) => !excludedOptions.includes(value),
-    );
-    option.values = filteredValues;
-  });
+    variants?.forEach((variant: ProductVariant) => {
+      // @ts-ignore
+      if (variant.variantExcluded && variant.variantExcluded.value === 'true') {
+        excludedVariantIds.push(variant.id);
+        if (variant.selectedOptions[0])
+          excludedOptions.push(variant.selectedOptions[0].value);
+        if (variant.selectedOptions[1])
+          excludedOptions.push(variant.selectedOptions[1].value);
+      }
+    });
+    // remove variants
+    // filteredVariants?.filter(
+    //   (variant: ProductVariant) => !excludedVariantIds.includes(variant.id),
+    // );
+    // remove options from options
+    // @ts-ignore
+    options?.forEach((option: {name: string; values: string[]}) => {
+      const filteredValues = option.values.filter(
+        (value: string) => !excludedOptions.includes(value),
+      );
+      option.values = filteredValues;
+    });
+
+    return excludedVariantIds;
+  }, [variants, options]);
 
   return {excludedVariantIds};
 }
