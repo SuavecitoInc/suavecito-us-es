146e6446f5a562253afd895aa15a15c2752f35d9
j-Riv <jose.alfredo.rivera@gmail.com>
2022-09-20 18:03:13 -0700

    Moved all one of product templates from routes -> templates, for consistency

--- src/components/templates/index.server.ts
+++ src/components/templates/index.server.ts
@@ -1,3 +1,4 @@
+export {ProductTemplate} from './product-template.server';
 export {ProductMetafieldPomadeTemplate} from './product-metafield-pomade-template.server';
 export {ProductMetafieldColorsGetInspiredTemplate} from './product-metafield-colors-get-inspired-template.server';
 export {ProductMetafieldGetInspiredTemplate} from './product-metafield-get-inspired-template.server';
