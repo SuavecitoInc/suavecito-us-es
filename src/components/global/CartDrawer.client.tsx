f130a7f4f03052f874614e655c2b615be19483fc
j-Riv <jose.alfredo.rivera@gmail.com>
2022-09-29 16:38:30 -0700

    Cart => Carrito

--- src/components/global/CartDrawer.client.tsx
+++ src/components/global/CartDrawer.client.tsx
@@ -10,13 +10,20 @@ export function CartDrawer({
   isOpen: boolean;
   onClose: () => void;
 }) {
+  const LANG = import.meta.env.PUBLIC_LANGUAGE_CODE;
+
   const {enabled, freeGiftsInCart, freeGiftsEligible, currentTier, tier1Diff} =
     useFreeGiftWithPurchase();
 
   const freeGiftAvailable = freeGiftsInCart < freeGiftsEligible[currentTier];
 
   return (
-    <Drawer open={isOpen} onClose={onClose} heading="Cart" openFrom="right">
+    <Drawer
+      open={isOpen}
+      onClose={onClose}
+      heading={LANG === 'es' ? 'Carrito' : 'Cart'}
+      openFrom="right"
+    >
       <div className="grid">
         <CartDetails
           layout="drawer"
