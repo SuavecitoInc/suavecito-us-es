4d301b7737301c79cb73284b313461439e71ccb5
j-Riv <jose.alfredo.rivera@gmail.com>
2022-10-11 17:50:29 -0700

    Moved Entry Client To JSX

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react';
 import {BrowserTracing} from '@sentry/tracing';
 import renderHydrogen from '@shopify/hydrogen/entry-client';
 
-const ClientWrapper = ({children}: {children: any}) => children;
+const ClientWrapper = ({children}) => children;
 
 Sentry.init({
   dsn: 'https://a69eb81eb70042308b425947369066d5@o1181994.ingest.sentry.io/4503967682789376',
@@ -11,7 +11,7 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.2,
 });
 
 export default renderHydrogen(ClientWrapper, {});
