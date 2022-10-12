dae34ebd499ed18f1652d381920b6fc5c4a236a6
j-Riv <jose.alfredo.rivera@gmail.com>
2022-10-11 17:08:06 -0700

    Added: Sentry

---
+++ src/entry-client.tsx
@@ -0,0 +1,18 @@
+import {ReactNode} from 'react';
+import * as Sentry from '@sentry/react';
+import {BrowserTracing} from '@sentry/tracing';
+import renderHydrogen from '@shopify/hydrogen/entry-client';
+
+const ClientWrapper = ({children}: {children: ReactNode}) => children;
+
+Sentry.init({
+  dsn: 'https://a69eb81eb70042308b425947369066d5@o1181994.ingest.sentry.io/4503967682789376',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+export default renderHydrogen(ClientWrapper, {});
