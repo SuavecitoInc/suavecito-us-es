8f569ba6535e035eeabadb4ec37f14c3bb0e1196
j-Riv <jose.alfredo.rivera@gmail.com>
2022-11-17 18:44:30 -0800

    Changes added temp workaround for Shopify Analytics Cart Provider error. Hyrdogen PR will fix this issue in future versions. Enabled mystery tee for testing

--- src/data/free-gift-with-purchase.ts
+++ src/data/free-gift-with-purchase.ts
@@ -1,8 +1,10 @@
 // Free Gift With Purchase
-export const FGWP_ENABLED = false;
-export const FGWP_SINGLE_TIER_ENABLED = false;
-export const FGWP_1 = 'wood-pulp-oil-blotting-sheets-fgwp';
-export const FGWP_2 = 'penny-folding-comb-quartz-fgwp';
+export const FGWP_ENABLED = true;
+export const FGWP_SINGLE_TIER_ENABLED = true;
+export const FGWP_1 = 'mens-mystery-tee';
+export const FGWP_2 = 'womens-mystery-tee';
+// export const FGWP_1 = 'wood-pulp-oil-blotting-sheets-fgwp';
+// export const FGWP_2 = 'penny-folding-comb-quartz-fgwp';
 export const FGWP_3 = 'lipgrip-bruja-fgwp';
 export const FGWP_4 = 'premium-blends-aftershave-balm-fgwp';
 export const FGWP_TIER_1_MIN = 55;
