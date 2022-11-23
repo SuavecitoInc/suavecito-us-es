296a9780650e984623b5e9a2b679179dd808d7ae
j-Riv <jose.alfredo.rivera@gmail.com>
2022-11-23 11:49:46 -0800

    Added Cart Banner

--- src/routes/cart.server.tsx
+++ src/routes/cart.server.tsx
@@ -3,10 +3,13 @@ import {
   PageHeader,
   CartPageDetails,
   CartFreeGiftWithPurchase,
+  ResponsiveBanner,
 } from '~/components';
 import {Layout} from '~/components/index.server';
 
 import {FGWP_ENABLED} from '~/data/free-gift-with-purchase';
+
+import {ENABLE_CART_BANNER, responsiveBannerSettings} from '../data/cart';
 
 export default function Cart() {
   const LANG = import.meta.env.PUBLIC_LANGUAGE_CODE;
@@ -16,6 +19,7 @@ export default function Cart() {
   return (
     <Layout>
       <Seo type="page" data={{title: 'Cart'}} />
+      {ENABLE_CART_BANNER && <ResponsiveBanner {...responsiveBannerSettings} />}
       <PageHeader heading={title} className="mx-auto max-w-7xl" />
       <CartPageDetails layout="page" />
       {FGWP_ENABLED && <CartFreeGiftWithPurchase />}
