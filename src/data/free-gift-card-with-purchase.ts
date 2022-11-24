e9b2db60990ec924bafebb133842c41e25ced079
j-Riv <jose.alfredo.rivera@gmail.com>
2022-11-23 18:20:25 -0800

    Disabled FGCWP

--- src/data/free-gift-card-with-purchase.ts
+++ src/data/free-gift-card-with-purchase.ts
@@ -1,5 +1,5 @@
 // Free Gift Card With Purchase
-export const FGCWP_ENABLED = true;
+export const FGCWP_ENABLED = false;
 export const FGCWP_1 = 'gift-card-10';
 export const FGCWP_2 = 'gift-card-25';
 export const FGCWP_TIER_1_MIN = 50;
