2799cf74b0c94977976ab98320c09a2b394ac485
j-Riv <jose.alfredo.rivera@gmail.com>
2022-12-26 13:08:00 -0800

    Updates

--- src/data/home-page/index.ts
+++ src/data/home-page/index.ts
@@ -1,4 +1,4 @@
 // export * from './home-page-es';
 // export * from './home-page-es-december';
-export * from './home-page-es-december-xmas';
-// export * from './home-page-es-december-after-xmas';
+// export * from './home-page-es-december-xmas';
+export * from './home-page-es-december-after-xmas';
